7f41a2ca7633d485a9925bd5e9cf3d3d276fd415
Ayush Singh <112885157+Ayush200Singh@users.noreply.github.com>
2022-09-30 12:41:39 +0530

    Update VersionDemo.java

--- VersionDemo.java
+++ VersionDemo.java
@@ -1,15 +1,12 @@
-/*
-Program: Check wheher the character is "Vowel","Consonent","Number","Specaial character"
-@author: Ayush singh
-@date: 29 sept 2022
-*/
-package corejava;
 
+package corejava;
+// Code written by Ayush Singh
+// program to find the version of  java 
 public class VesionofJava {
 
 	public static void main(String[] args) {
 	  //printing version of java 
-		System.out.println("Version of java is:"+ System.getProperty("java.version"));
+		System.out.println("Version of java is:"+ System.getProperty("java.version"));  
 		
 
 	}
