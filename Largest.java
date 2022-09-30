4cbd414272aa810db86dea8f43fd09bba33fd5f2
Ayush Singh <112885157+Ayush200Singh@users.noreply.github.com>
2022-09-30 12:39:41 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -1,16 +1,16 @@
 package corejava;
-
+// Code written by Ayush Singh
+// Program to find Largest number
 import java.util.Scanner;
-
 public class Largest_number {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner (System.in);   // Use Scanner class To take Input from user
 		System.out.println("Enter first number");
 		int num = sc.nextInt();
 		System.out.println("Enter second number");
 		int num1 = sc.nextInt();
-		if(num<num1) {
+		if(num<num1) {  // logic to find largest number
 			System.out.println(  num1+" is largest number"+ num1);
 		}
 		else {
