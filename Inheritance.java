72a92599026abd47c32a43aefeed11bc77beabf6
Ayush Singh <112885157+Ayush200Singh@users.noreply.github.com>
2022-09-30 12:35:13 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -2,15 +2,15 @@ package corejava;
 // code written by Ayush singh
 // Show inheritance
 class Parents{
-	 void Parent(){
+	 void Parent(){   // default constructor
 		System.out.println("I am a default constructor");
 	}
-	 void Sing() {
+	 void Sing() {    
 		 System.out.println("Ram is singing");
 		 
 	 }
 }
-class Child extends Parents{
+class Child extends Parents{    // use extends keyword to inherit the property of parent class 
 	void Speak() {
 		System.out.println("RAM want to speak ");
 	}
@@ -20,8 +20,8 @@ class Child extends Parents{
 }
 public class Lab_test {
 	public static void main(String[] args) {
-		Child ch = new Child();
-		    ch.Parent();
+		Child ch = new Child();   // Making the object(ch) of Child class to call the methods of Child class
+		    ch.Parent();  
 		    ch.Sing();
 			ch.Dance();
 			ch.Speak();
