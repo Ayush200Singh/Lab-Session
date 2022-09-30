8972a879d28c030c585dfec8adc7baff12d48c10
Ayush Singh <112885157+Ayush200Singh@users.noreply.github.com>
2022-09-30 12:26:00 +0530

    Add files via upload

---
+++ VersionDemo.java
@@ -0,0 +1,17 @@
+/*
+Program: Check wheher the character is "Vowel","Consonent","Number","Specaial character"
+@author: Ayush singh
+@date: 29 sept 2022
+*/
+package corejava;
+
+public class VesionofJava {
+
+	public static void main(String[] args) {
+	  //printing version of java 
+		System.out.println("Version of java is:"+ System.getProperty("java.version"));
+		
+
+	}
+
+	}
